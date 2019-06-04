3a0a04a24b962ace895e13b8c52363be87325557
Luis Sánchez <Luis.Sanchez@ust-global.com>
2019-06-04 19:57:22 +0200

    CR

--- setup.sh
+++ setup.sh
@@ -1,18 +1,27 @@
-#!/bin/bash
-
-HUB_NAME=IoTHubLASV2
-
-printf "Installing Azure IoT extension ... (1/7)\n\n"
+printf "Installing Azure IoT extension ... \n\n"
 
 az extension add --name azure-cli-iot-ext
 
-printf "\nCreating identities for the edge devices ... (2/7)\n\n"
+printf "\nCreating identities for the edge devices ...\n\n"
 
-az iot hub device-identity create --hub-name $HUB_NAME --device-id EdgeDevice1 --edge-enabled
-az iot hub device-identity create --hub-name $HUB_NAME --device-id EdgeDevice2 --edge-enabled
-CONN_STRING1=az iot hub device-identity show-connection-string --device-id EdgeDevice1 --hub-name $HUB_NAME
-CONN_STRING2=az iot hub device-identity show-connection-string --device-id EdgeDevice2 --hub-name $HUB_NAME
-az vm run-command invoke -g IoTEdgeResources -n EdgeVM1 --command-id RunShellScript --script '/etc/iotedge/configedge.sh "$CONN_STRING1"'
-az vm run-command invoke -g IoTEdgeResources -n EdgeVM2 --command-id RunShellScript --script '/etc/iotedge/configedge.sh "$CONN_STRING2"'
+az iot hub device-identity create --hub-name IoTHubLASV2 --device-id EdgeDevice1 --edge-enabled
+az iot hub device-identity create --hub-name IoTHubLASV2 --device-id EdgeDevice2 --edge-enabled
+connection1=$(az iot hub device-identity show-connection-string \
+    --device-id EdgeDevice1 \
+    --hub-name IoTHubLASV2)
+connection2=$(az iot hub device-identity show-connection-string \
+    --device-id EdgeDevice2 \
+    --hub-name IoTHubLASV2)
+
+printf "\nAdding device identities to VMs ...\n\n"
+
+az vm run-command invoke -g IoTEdgeResources -n EdgeVM1 --command-id RunShellScript --script '/etc/iotedge/configedge.sh "connection1"'
+az vm run-command invoke -g IoTEdgeResources -n EdgeVM2 --command-id RunShellScript --script '/etc/iotedge/configedge.sh "connection2"'
+
+printf "\nAdding tags to devices ...\n\n"
+
+az iot hub device-twin update --device-id EdgeDevice1 --hub-name IoTHubLASV2 --set tags='{"location":{"region":"ES"}}'
+az iot hub device-twin update --device-id EdgeDevice2 --hub-name IoTHubLASV2 --set tags='{"location":{"region":"ES"}}'
+
 
 printf "Setup complete!\n\n"
